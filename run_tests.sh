09c1fce356604607c8a046981d26a403ae7434ca
J Fixby <github@jfixby.com>
2019-03-14 19:56:49 +0100

    try pfcd setup

--- run_tests.sh
+++ run_tests.sh
@@ -1,69 +1,66 @@
-#!/usr/bin/env bash
 set -ex
 
-# The script does automatic checking on a Go package and its sub-packages,
-# including:
-# 1. gofmt         (http://golang.org/cmd/gofmt/)
-# 2. go vet        (http://golang.org/cmd/vet)
-# 3. gosimple      (https://github.com/dominikh/go-simple)
-# 4. unconvert     (https://github.com/mdempsky/unconvert)
-# 5. ineffassign   (https://github.com/gordonklaus/ineffassign)
-# 6. race detector (http://blog.golang.org/race-detector)
-
-# gometalinter (github.com/alecthomas/gometalinter) is used to run each each
-# static checker.
-
-GOVERSION=${1:-1.11}
+# Default GOVERSION
+[[ ! "$GOVERSION" ]] && GOVERSION=1.11
 REPO=pfcdata
-DOCKER_IMAGE_TAG=picfight-golang-builder-$GOVERSION
 
 testrepo () {
-  TMPFILE=$(mktemp)
-  export GO111MODULE=on
-
-  go version
-  go get ./...
-  go build -v
-
-  # Test application install
-  go install . ./cmd/...
-  if [ $? != 0 ]; then
-    echo 'go install failed'
-    exit 1
+  GO=go
+  if [[ $GOVERSION == 1.10 ]]; then
+    GO=vgo
   fi
 
-  # Check tests
-  git clone https://github.com/dcrlabs/bug-free-happiness test-data-repo
-  tar xvf test-data-repo/stakedb/test_ticket_pool.bdgr.tar.xz -C ./stakedb
+  $GO version
 
-  env GORACE='halt_on_error=1' go test -v -race ./...
-  if [ $? != 0 ]; then
-    echo 'go tests failed'
-    exit 1
+  # binary needed for RPC tests
+  env CC=gcc $GO build
+  cp "$REPO" "$GOPATH/bin/"
+
+  # run tests on all modules
+  ROOTPATH=$($GO list -m -f {{.Dir}} 2>/dev/null)
+  ROOTPATHPATTERN=$(echo $ROOTPATH | sed 's/\\/\\\\/g' | sed 's/\//\\\//g')
+  MODPATHS=$($GO list -m -f {{.Dir}} all 2>/dev/null | grep "^$ROOTPATHPATTERN"\
+    | sed -e "s/^$ROOTPATHPATTERN//" -e 's/^\\//' -e 's/^\///')
+  MODPATHS=". $MODPATHS"
+  for module in $MODPATHS; do
+    echo "==> ${module}"
+    (cd $module && env GORACE='halt_on_error=1' CC=gcc $GO test -short -race \
+	  -tags rpctest ./...)
+  done
+
+  # check linters
+  if [[ $GOVERSION != 1.10 ]]; then
+    # linters do not work with modules yet
+    go mod vendor
+    unset GO111MODULE
+
   fi
 
   echo "------------------------------------------"
   echo "Tests completed successfully!"
 }
 
-if [ $GOVERSION == "local" ]; then
+DOCKER=
+[[ "$1" == "docker" || "$1" == "podman" ]] && DOCKER=$1
+if [ ! "$DOCKER" ]; then
     testrepo
     exit
 fi
 
-docker pull picfight/$DOCKER_IMAGE_TAG
-if [ $? != 0 ]; then
-        echo 'docker pull failed'
-        exit 1
+# use Travis cache with docker
+DOCKER_IMAGE_TAG=picfight-golang-builder-$GOVERSION
+mkdir -p ~/.cache
+if [ -f ~/.cache/$DOCKER_IMAGE_TAG.tar ]; then
+  # load via cache
+  $DOCKER load -i ~/.cache/$DOCKER_IMAGE_TAG.tar
+else
+  # pull and save image to cache
+  $DOCKER pull picfight/$DOCKER_IMAGE_TAG
+  $DOCKER save picfight/$DOCKER_IMAGE_TAG > ~/.cache/$DOCKER_IMAGE_TAG.tar
 fi
 
-docker run --rm -it -v $(pwd):/src picfight/$DOCKER_IMAGE_TAG /bin/bash -c "\
-  rsync -ra --include-from=<(git --git-dir=/src/.git ls-files) \
-  --filter=':- .gitignore' \
+$DOCKER run --rm -it -v $(pwd):/src:Z picfight/$DOCKER_IMAGE_TAG /bin/bash -c "\
+  rsync -ra --filter=':- .gitignore'  \
   /src/ /go/src/github.com/picfight/$REPO/ && \
-  ls && \
-  bash run_tests.sh local"
-if [ $? != 0 ]; then
-        echo 'docker run failed'
-        exit 1
-fi
+  dir && \
+  env GOVERSION=$GOVERSION GO111MODULE=on bash run_tests.sh"
